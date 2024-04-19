fa36d8481f551cf812c645fdfe5400fc3c9cc7e2
Tomáš Mäsiar <tomi.masiar@gmail.com>
2024-04-19 15:31:41 +0200

    styling changes

--- pokedex/src/components/Navbar.js
+++ pokedex/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import myImage from '../pokedex_logo.png';
 
 function Navbar() {
     return(
         <nav className="navbar">
             <div className="navbar-container">
-                <div className="logo">Pokédex</div>
+                <div><img src={myImage} alt='Pokedex' className="logo"/></div>
                 <ul className="nav-links">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
--- pokedex/src/components/PokemonDetails.js
+++ pokedex/src/components/PokemonDetails.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 const PokemonDetails = () => {
     const { id } = useParams();
     const [pokemonData, setPokemonData] = useState(null);
+    const getTypeColorName = (typeName) => 'type-'+typeName;
 
     useEffect(() => {
     const fetchData = async () => {
@@ -32,26 +33,37 @@ const PokemonDetails = () => {
         <>
         <Navbar />
         <div className="pokemon-container">
-        <h2>{pokemonData.name}</h2>
+        <div className="pokemon-card">
+        <h2>{pokemonData.name.toUpperCase()}</h2>
+        <div className="horizontal-line"></div>
+
+        <div className="pokemon-card-header">
+        <div className="pokemon-image-container">
         <img
         src={pokemonData.sprites.front_default}
         alt={pokemonData.name}
         className="pokemon-image"
         />
+        </div>
+        <div className="pokemon-types-container">
+        <h3>Types:</h3>
+        <ul className="pokemon-list-types">
+        {pokemonData.types.map((type, index) => (
+            <li key={index} className={getTypeColorName(type.type.name)}>{type.type.name}</li>
+        ))}
+        </ul>
+        </div>
+        </div>
+        
         <h3>Abilities:</h3>
-        <ul className="pokemon-list">
+        <ul className="pokemon-list-abilities">
         {pokemonData.abilities.map((ability, index) => (
             <li key={index}>{ability.ability.name}</li>
         ))}
         </ul>
-        <h3>Types:</h3>
-        <ul className="pokemon-list">
-        {pokemonData.types.map((type, index) => (
-            <li key={index}>{type.type.name}</li>
-        ))}
-        </ul>
+        
         <h3>Stats:</h3>
-        <ul className="pokemon-list">
+        <ul className="pokemon-list-stats">
         {pokemonData.stats.map((stat, index) => (
             <li key={index}>
             {stat.stat.name}: {stat.base_stat}
@@ -59,11 +71,12 @@ const PokemonDetails = () => {
         ))}
         </ul>
         <h3>Moves:</h3>
-        <ul className="pokemon-list">
+        <ul className="pokemon-list-moves">
         {pokemonData.moves.slice(0, 5).map((move, index) => (
             <li key={index}>{move.move.name}</li>
         ))}
         </ul>
+        </div>
     </div>
     </>
     );
