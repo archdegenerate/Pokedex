1b16678aa8ac46f3f067975c269ae766c990128a
Tomáš Mäsiar <tomi.masiar@gmail.com>
2024-04-26 10:01:12 +0200

    fix search to move items to the front of the catalogue

--- pokedex/src/components/PokemonList.js
+++ pokedex/src/components/PokemonList.js
@@ -1,40 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import '../App.css';
 
 function PokemonList() {
     const [pokemonData, setPokemonData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [searchQuery, setSearchQuery] = useState('');
     const LIMIT = 9;
     const getTypeColorName = (typeName) => 'pokemon-type-'+typeName;
 
-
     useEffect(() => {
-        const fetchData = async (offset, LIMIT) => {
-        try {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${LIMIT}`);
-            const data = await response.json();
-            setTotalPages(Math.ceil(data.count / LIMIT));
-
-            const pokemonDetails = await Promise.all(
-                data.results.map(async (pokemon) => {
-                const pokemonResponse = await fetch(pokemon.url);
-                return pokemonResponse.json();
-                })
-            );
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=964`);
+                if (!response.ok) throw new Error('Failed to fetch Pokémon data');
+                const data = await response.json();
     
-            setPokemonData(pokemonDetails);
+                const pokemonDetails = await Promise.all(
+                    data.results.map(async (pokemon) => {
+                        const pokemonResponse = await fetch(pokemon.url);
+                        if (!pokemonResponse.ok) throw new Error(`Failed to fetch data for ${pokemon.name}`);
+                        const pokemonData = await pokemonResponse.json();
+                        return pokemonData;
+                    })
+                );
+    
+                setPokemonData(pokemonDetails);
+                setTotalPages(Math.ceil(data.results.length / LIMIT));
             } catch (error) {
-            console.error('Error fetching data:', error);
+                console.error('Error fetching data:', error);
             }
         };
     
-        fetchData((currentPage - 1) * LIMIT, LIMIT);
-        }, [currentPage, LIMIT]);
+        fetchData();
+    }, []);
+    
+
+    useEffect(() => {
+        if (searchQuery) {
+            const results = pokemonData.filter(pokemon => 
+                pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setFilteredData(results);
+            setTotalPages(Math.ceil(results.length / LIMIT));
+            setCurrentPage(1);
+        } else {
+            setFilteredData(pokemonData);
+            setTotalPages(Math.ceil(pokemonData.length / LIMIT));
+        }
+    }, [searchQuery, pokemonData]);
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -52,8 +69,8 @@ function PokemonList() {
         <>
             <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
             <div className="pokemon-list-container">
-            {pokemonData
-                    .filter(pokemon => pokemon.name.toLowerCase().includes(searchQuery))
+                {filteredData
+                    .slice((currentPage - 1) * LIMIT, currentPage * LIMIT)
                     .map((pokemon) => (
                     <Link
                         to={'/pokemon/' + pokemon.id} 
@@ -61,29 +78,28 @@ function PokemonList() {
                         className="pokemon-item"
                         id={pokemon.types && pokemon.types.length > 0 ? getTypeColorName(pokemon.types[0].type.name) : 'pokemon-type-default'}
                     >
-                <div className="pokemon-item">
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                <div className="pokemon-details">
-                    <h2>{pokemon.name}</h2>
-                    <p>ID: {pokemon.id}</p> 
-                </div>
-                </div>
-            </Link>
-            ))}
+                        <div className="pokemon-item">
+                        <img src={pokemon.sprites?.front_default || 'path/to/placeholder/image.png'} alt={pokemon.name} />
+                            <div className="pokemon-details">
+                                <h2>{pokemon.name}</h2>
+                                <p>ID: {pokemon.id}</p> 
+                            </div>
+                        </div>
+                    </Link>
+                ))}
             </div>
             <div className="button-container">
-            <div>
-                <button onClick={handlePrevPage} disabled={currentPage <= 1} className="pokemon-list-button">
-                    ←
-                </button>
+                <div>
+                    <button onClick={handlePrevPage} disabled={currentPage <= 1} className="pokemon-list-button">
+                        ←
+                    </button>
+                </div>
+                <div>
+                <button onClick={handleNextPage} disabled={currentPage >= totalPages} className="pokemon-list-button">
+                    →
+                    </button>
+                </div>
             </div>
-            <div>
-            <button onClick={handleNextPage} disabled={currentPage >= totalPages} className="pokemon-list-button">
-                →
-            </button>
-            </div>
-        </div>
-            
         </>
     )
 }
